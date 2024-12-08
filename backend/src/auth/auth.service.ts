d6c9c2994527310728cef02c94b75ec4bd89e164
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-12-08 20:49:57 +0100

    feat: include role in user

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,31 +1,50 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import { User } from 'src/users/users.schema';
+import { User, UserDocument } from 'src/users/users.schema';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
   ) {}
 
-  async login(loginDto: { email: string; password: string }): Promise<{ name: string; firstname: string; }> {
+  async login(loginDto: {
+    email: string;
+    password: string;
+  }): Promise<Partial<UserDocument>> {
     const user = await this.usersService.findOneByEmail(loginDto.email);
-    
-    if (!user || !(await this.usersService.validatePassword(user, loginDto.password))) {
+
+    if (
+      !user ||
+      !(await this.usersService.validatePassword(user, loginDto.password))
+    ) {
       throw new Error('Invalid credentials');
     }
 
-    return { name: user.name, firstname: user.name};
+    return {
+      _id: user._id,
+      name: user.name,
+      firstname: user.firstname,
+      email: user.email,
+      role: user.role,
+    };
   }
 
   async logout() {
     return { message: 'Logout successful' };
   }
 
-  async signup(user: Partial<User>): Promise<User> {
-    return this.usersService.create(user);
+  async signup(user: Partial<User>): Promise<Partial<UserDocument>> {
+    const newUser = await this.usersService.create(user).then((user) => user.populate('role'));
+      return {
+        _id: newUser._id,
+        name: newUser.name,
+        firstname: newUser.firstname,
+        email: newUser.email,
+        role: newUser.role
+    }
   }
 
   async generateJwt(payload: any): Promise<string> {
--- backend/src/roles/roles.schema.ts
+++ backend/src/roles/roles.schema.ts
@@ -1,8 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export type RoleDocument = Role & Document;
+
 @Schema()
-export class Role extends Document {
+export class Role {
   @Prop({ required: true })
   name: string;
 
